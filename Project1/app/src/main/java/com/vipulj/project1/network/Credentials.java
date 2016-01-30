05c484973547a7dc4be4c4cec20e768f9d8c0df1
Vipul J <messagevipul@gmail.com>
2016-01-31 01:11:18 +0800

    API Key removed

--- Project1/app/src/main/java/com/vipulj/project1/network/Credentials.java
+++ Project1/app/src/main/java/com/vipulj/project1/network/Credentials.java
@@ -4,5 +4,6 @@ package com.vipulj.project1.network;
  * Created by VJ on 11/01/16.
  */
 public class Credentials {
-    public static final String MOVIE_DB_API_KEY = "59632cb728cdf1269b49f2b029f56453";
+//     Enter API KEY here
+    public static final String MOVIE_DB_API_KEY = "";
 }
